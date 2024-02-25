cf34be4258f1a4e1cacb5406860f20e4400e4644
vishal mahajan <mahajanxvishal@gmail.com>
2024-02-25 14:39:58 +0530

    deployed frontend succesfully

--- frontend/src/components/Reservation.jsx
+++ frontend/src/components/Reservation.jsx
@@ -28,7 +28,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/reservation/send",
+        "https://mern-bhojanalay.vercel.app/api/v1/reservation/send",
         { firstName, lastName, email, phone, date, time },
         {
           headers: {
